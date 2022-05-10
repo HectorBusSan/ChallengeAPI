57d36208db1acaa421329ddd6198bc3c1aa80ac6
Hector <hector141599@gmail.com>
2022-05-09 19:51:19 -0500

    Inciando los test

---
+++ test/services/VisualServices.test.js
@@ -0,0 +1,10 @@
+const VisualServices = require("../../lib/services/VisualServices");
+const Reader = require("../../lib/utils/Reader");
+describe('Test para VisualServices',()=>){
+    test('Requerimiento 1: Encontrar a los Estudiantes con el correo confirmado',()=>{
+
+    });
+    test('Requerimiento 2: Encontrar a los estudiantes con creditos por arriba de los 500',()=>{
+
+    });
+}
